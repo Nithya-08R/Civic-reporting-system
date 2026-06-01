09e76f15bb330639a68a7839a0f25dd188145a76
Nithya <nithyaraju003@gmail.com>
2026-06-01 13:56:05 +0530

    Fixed Image temp

--- Backend/server.js
+++ Backend/server.js
@@ -31,6 +31,15 @@ app.use("/api/notifications", require("./routes/notificationRoutes"));
 const feedbackRoute = require("./routes/feedbackRoute");
 app.use("/api/feedback", feedbackRoute);
 
+const fs = require("fs");
+
+app.get("/check-uploads", (req, res) => {
+  fs.readdir("uploads", (err, files) => {
+    if (err) return res.json(err);
+    res.json(files);
+  });
+});
+
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
